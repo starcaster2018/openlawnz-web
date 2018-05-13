5311e9e1d746476b8e1839857e13a0b01cea618f
Andrew <easterbrook@gmail.com>
2018-05-13 22:51:52 +1200

    Fix revert

--- src/jsx/Home.jsx
+++ src/jsx/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Container, Segment, Header, Item, Image, List } from 'semantic-ui-react'
+import { Button, Container, Segment, Header, Item, Image } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 import Placeholder from '../img/placeholder.png';
 
@@ -9,29 +9,12 @@ class Home extends Component {
 			
 			<React.Fragment>
 				
-				<Header as='h2'>Welcome to OpenLaw NZ</Header>
-				<p>OpenLaw NZ is a free, open source, legal research platform.</p>
-
-				<p>We have analysed thousands of cases to find structure and relationships between different cases, and relationships between legislation and cases.</p>
-
-				<p>Use the case search to find a case, and find related cases. Use our <a href="">chrome extension</a> and visit <a href="https://www.legislation.govt.nz">legislation.govt.nz</a> to find cases that relate to a section of an Act.</p>
-
-				<p>There is an API. The API allows you to access our database directly, and build your own applications using OpenLaw NZ data.</p> 
+				<Header as='h2'>Welcome</Header>
 				
-				<Header as='h2'>Our Principles</Header>
-
-				<List bulleted>
-					<List.Item>Access to legal information should be free</List.Item>
-					<List.Item>Legal information should be accessible					
-					<List.List>
-						<List.Item>For humans and</List.Item>
-						<List.Item>For computers</List.Item>
-					</List.List>
-					</List.Item>
-					<List.Item>Legal research software should be open-source</List.Item>
-				</List>
+				<p>We have analysed thousands of cases and made connections between them and associated legislation.</p>
 				
-				<Header as='h2'>Relationship Search</Header>
+				<Header as='h3'>Metadata</Header>
+				
 				<Item.Group>
 					<Item>
 						<Item.Image size='tiny' src={Placeholder} />

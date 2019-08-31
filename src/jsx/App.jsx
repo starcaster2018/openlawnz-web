0827635779e3f0415dbd9e37e05d2cffa122645d
blaineanthonywestern <blaine.a.western@gmail.com>
2019-08-31 12:45:23 +1200

    add styling and remove semantic ui

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -9,12 +9,12 @@ import Plugins from "./pages/Plugins.jsx";
 import Developers from "./pages/Developers.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
+import Footer from "./components/Footer.jsx"
 import { Container, Header, Segment } from "semantic-ui-react";
 
 import "semantic-ui-css/semantic.min.css";
 import "../scss/App.scss";
 
-import Logo from "-!svg-react-loader?name=Logo!../img/openlaw-logo.svg";
 
 class App extends Component {
 	render() {
@@ -22,17 +22,9 @@ class App extends Component {
 
 		return (
 			<Router>
-				<Container>
-					<div className="main-header">
-						<Header as="h1" icon textAlign="center">
-							<Logo alt="OpenLaw NZ" className="main-logo" />
-							<Header.Subheader>New Zealand case law and legislation metadata platform</Header.Subheader>
-						</Header>
-					</div>
-
+				<div>	
 					<MainNavWithRouter />
-
-					<Segment attached="bottom" padded="very">
+					<div className="content-wrapper">
 						<Route exact path="/" component={Home} />
 						<Route exact path="/search" component={Search} />
 						<Route exact path="/case/:id" component={SingleCase} />
@@ -40,16 +32,9 @@ class App extends Component {
 						<Route exact path="/plugin" component={Plugins} />
 						<Route exact path="/contact" component={Contact} />
 						<Route exact path="/about" component={About} />
-					</Segment>
-					<footer>
-						<p>
-							&copy; 2018 OpenLaw NZ |{" "}
-							<a href="https://www.register.charities.govt.nz/Charity/CC55967" rel="noopener noreferrer">
-								Registered NZ Charity
-							</a>
-						</p>
-					</footer>
-				</Container>
+						<Footer/>
+					</div>
+				</div>
 			</Router>
 		);
 	}

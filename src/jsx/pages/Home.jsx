cd945bc1e93db3b6eb264fa398cfa5f13eb5280b
Caio Navarro <caionavarro.n@hotmail.com>
2019-09-02 21:07:17 -0300

    Add jsx of 'our mission' back to home page

--- src/jsx/pages/Home.jsx
+++ src/jsx/pages/Home.jsx
@@ -8,9 +8,39 @@ import MissionFundLogo from "-!svg-react-loader!../../img/mission-fund.svg";
 class Home extends Component {
 	render() {
 		return (
-			<div className="home-wrapper">
-				<Search/>
-			</div>
+			<React.Fragment>
+				<div className="home-wrapper">
+					<Search/>
+				</div>
+				<div className='container'>
+					<h2 className="section-title is-text-center">Our Mission</h2>
+					<h3 className="section-subtitle is-text-center">OpenLaw NZ is an open-source platform that any country can copy and use for the betterment of their citizens.</h3>
+
+					<div className='horizontal-container horizontal-container--spaced'>
+						<div className='horizontal-container__item horizontal-container__item--small'>
+							<div className='card'>
+								<MissionAccessLogo className='mission-icon' />
+								<p>Improve access to justive and education for all New Zealanders</p>
+							</div>
+						</div>
+
+						<div className='horizontal-container__item horizontal-container__item--small'>
+							<div className='card'>
+								<MissionInnovationLogo className='mission-icon' />
+								<p>Spur innovation with a freely available relational database</p>
+							</div>
+						</div>
+
+						<div className='horizontal-container__item horizontal-container__item--small'>
+							<div className='card'>
+								<MissionFundLogo className='mission-icon' />
+								<p>Fund future development by providing services using the OpenLaw NZ platform</p>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</React.Fragment>
 		);
 	}
 }

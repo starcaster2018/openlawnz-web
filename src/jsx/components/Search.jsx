e0a1345b0d0a71fd48423d2f5a9899ac268d8752
blaineanthonywestern <blaine.a.western@gmail.com>
2019-09-03 17:19:27 +1200

    add styling

--- src/jsx/components/Search.jsx
+++ src/jsx/components/Search.jsx
@@ -7,12 +7,39 @@ export default class Search extends Component {
         return (
             <div>
             <div class="search">
-             <div class="search-input">
+            <div class="search-input">
             <input type="text" class="search-term" placeholder="Search legal cases" />
             <button type="submit" class="search-button"><SearchIcon /></button>
-                </div>
+            </div>
             <button type="submit" class="search-submit-button">Search</button>
-                </div>
+            </div>
+            <div className="advanced-search">
+            <div className="advanced-search-unit"> 
+            <select>
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="vw">VW</option>
+            <option value="audi" selected>Audi</option>
+            </select>
+            </div>   
+            <div className="advanced-search">
+            <div className="advanced-search-unit"> 
+            <select>
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="vw">VW</option>
+            <option value="audi" selected>Audi</option>
+            </select>
+            </div>
+            <div className="advanced-search-unit"> 
+            <select>
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="vw">VW</option>
+            <option value="audi" selected>Audi</option>
+            </select>
+            </div>
+            </div>
             </div>
         )
     }

c29afe1127fd3c9231d50fb3782196545f232aa7
Caio Navarro <caionavarro.n@hotmail.com>
2019-09-25 08:49:14 -0300

    Improve readability of SingCaseView by adding variables and arrow functions

--- src/jsx/components/SingleCaseView.jsx
+++ src/jsx/components/SingleCaseView.jsx
@@ -7,29 +7,29 @@ import Open from "-!svg-react-loader?name=Logo!../../img/open-details.svg";
 import Close from "-!svg-react-loader?name=Logo!../../img/close-details.svg";
 
 class SingleCaseView extends Component {
-	constructor({ match }) {
+	constructor() {
 		super();
 		this.state = {
-			details: false
+			showDetails: false
 		};
 	}
 
 	hideShowDetails() {
-		this.setState({ details: !this.state.details });
+		this.setState({ showDetails: !this.state.showDetails });
 	}
 
 	render() {
+		const { pdf = {}, caseCitations, cites, casesCitedsByCaseCited, legislationToCases } =
+			this.props.singleCase || {};
+
 		return (
 			<div className="single-case-wrapper">
 				<div className="single-case-header">
 					<div className="details-open-close-button" onClick={this.hideShowDetails.bind(this)}>
-						info {this.state.details ? <Close /> : <Open />}
+						info {this.state.showDetails ? <Close /> : <Open />}
 					</div>
 					<div className="download-button">
-						<a
-							href={`https://s3-ap-southeast-2.amazonaws.com/openlawnz-pdfs/${this.props.singleCase.pdf.pdfDbKey}`}
-							download
-						>
+						<a href={`https://s3-ap-southeast-2.amazonaws.com/openlawnz-pdfs/${pdf.pdfDbKey}`} download>
 							<Download alt="Download" className="download-icon" />
 						</a>
 					</div>
@@ -37,75 +37,65 @@ class SingleCaseView extends Component {
 				<div className="row">
 					<div className="case-document-viewer">
 						<iframe
-							src={`https://docs.google.com/gview?url=https://s3-ap-southeast-2.amazonaws.com/openlawnz-pdfs/${this.props.singleCase.pdf.pdfDbKey}&embedded=true`}
+							src={`https://docs.google.com/gview?url=https://s3-ap-southeast-2.amazonaws.com/openlawnz-pdfs/${pdf.pdfDbKey}&embedded=true`}
 							frameBorder={0}
 						/>
 					</div>
-					<div className={this.state.details ? "case-details" : "hide-case-details"}>
+					<div className={this.state.showDetails ? "case-details" : "hide-case-details"}>
 						<h3 className="header">Citations known for this case</h3>
-						{this.props.singleCase.caseCitations &&
-							(this.props.singleCase.caseCitations === 0 ? (
+						{caseCitations &&
+							(caseCitations === 0 ? (
 								<p>None</p>
 							) : (
 								<div role="listitem" className="item">
-									{this.props.singleCase.caseCitations.map(function(obj) {
-										return (
-											<div
-												role="listitem"
-												className="item"
-												key={`cites-reference-${obj.citation}`}
-											>
-												{obj.citation}
-											</div>
-										);
-									})}
+									{caseCitations.map(obj => (
+										<div role="listitem" className="item" key={`cites-reference-${obj.citation}`}>
+											{obj.citation}
+										</div>
+									))}
 								</div>
 							))}
 						<hr></hr>
 
 						<h3 className="header">Cites</h3>
-						{this.props.singleCase.cites &&
-							(this.props.singleCase.cites.length === 0 ? (
+						{cites &&
+							(cites.length === 0 ? (
 								<p>No cases</p>
 							) : (
 								<div role="listitem" className="item">
-									{this.props.singleCase.cites.map(function(obj) {
-										return (
-											<div role="listitem" className="item" key={`cites-reference-${obj.id}`}>
-												<Link to={`/case/${obj.id}`}>{obj.caseName}</Link>
-											</div>
-										);
-									})}
+									{cites.map(obj => (
+										<div role="listitem" className="item" key={`cites-reference-${obj.id}`}>
+											<Link to={`/case/${obj.id}`}>{obj.caseName}</Link>
+										</div>
+									))}
 								</div>
 							))}
 						<hr></hr>
 
 						<h3 className="header">Cited by</h3>
-						{this.props.singleCase.casesCitedsByCaseCited &&
-							(this.props.singleCase.casesCitedsByCaseCited.length === 0 ? (
+						{casesCitedsByCaseCited &&
+							(casesCitedsByCaseCited.length === 0 ? (
 								<p>No cases</p>
 							) : (
 								<div role="listitem" className="item">
-									{this.props.singleCase.casesCitedsByCaseCited.map(function(obj) {
-										return (
-											<div
-												role="listitem"
-												className="item"
-												key={`cited-by-reference-${obj.caseByCaseOrigin.id}`}
-											>
-												<Link to={`/case/${obj.caseByCaseOrigin.id}`}>
-													{obj.caseByCaseOrigin.caseName}
-												</Link>
-											</div>
-										);
-									})}
+									{casesCitedsByCaseCited.map(obj => (
+										<div
+											role="listitem"
+											className="item"
+											key={`cited-by-reference-${obj.caseByCaseOrigin.id}`}
+										>
+											<Link to={`/case/${obj.caseByCaseOrigin.id}`}>
+												{obj.caseByCaseOrigin.caseName}
+											</Link>
+										</div>
+									))}
 								</div>
 							))}
 						<hr></hr>
 
 						<h3 className="header">Legislation Referenced</h3>
-						{this.props.singleCase.legislationToCases &&
-							(this.props.singleCase.legislationToCases.length === 0 ? (
+						{legislationToCases &&
+							(legislationToCases.length === 0 ? (
 								<p>No legislation</p>
 							) : (
 								<table cellSpacing="0" cellPadding="0">
@@ -116,14 +106,12 @@ class SingleCaseView extends Component {
 										</tr>
 									</thead>
 									<tbody className="">
-										{this.props.singleCase.legislationToCases.map(function(obj, i) {
-											return (
-												<tr className="" key={`legislation-reference-${i}`}>
-													<td className="">{obj.legislation.title}</td>
-													<td className="">{obj.section}</td>
-												</tr>
-											);
-										})}
+										{legislationToCases.map((obj, i) => (
+											<tr className="" key={`legislation-reference-${i}`}>
+												<td className="">{obj.legislation.title}</td>
+												<td className="">{obj.section}</td>
+											</tr>
+										))}
 									</tbody>
 								</table>
 							))}

1186e36fcf61bf9d9800f0488de111aa14b325e6
Caio Navarro <caionavarro.n@hotmail.com>
2019-10-09 19:26:54 -0300

    Improve scrollTo when SingleCase did mount

--- src/jsx/pages/SingleCase.jsx
+++ src/jsx/pages/SingleCase.jsx
@@ -14,6 +14,7 @@ class SingleCase extends Component {
 			singleCase: null,
 			loadingCase: true
 		};
+		this.titleRef = React.createRef();
 	}
 
 	async fetchData(id) {
@@ -24,7 +25,7 @@ class SingleCase extends Component {
 	async componentDidMount() {
 		this._isMounted = true;
 		this.fetchData(this.state.id);
-		window.scrollTo(0, 0);
+		window.scrollTo(0, this.titleRef.current.getBoundingClientRect().top + window.pageYOffset - 40);
 	}
 
 	async componentDidUpdate(prevProps) {
@@ -50,7 +51,10 @@ class SingleCase extends Component {
 				<Search history={this.props.history} />
 				<div className="home-wrapper">
 					<InfoCard>
-						<h2 className={this.state.loadingCase ? "header-case" : this.handleInfoCardHeaderSize()}>
+						<h2
+							ref={this.titleRef}
+							className={this.state.loadingCase ? "header-case" : this.handleInfoCardHeaderSize()}
+						>
 							{this.state.loadingCase ? "Loading..." : this.state.singleCase.caseName}
 						</h2>
 					</InfoCard>

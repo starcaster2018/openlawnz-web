0daf448b2dd1a1b536a25ee4ade1782fa4247c4e
Caio Navarro <caionavarro.n@hotmail.com>
2019-12-13 12:24:06 -0300

    Replace google pdf viewer with new Adobe Viewer

--- src/jsx/components/SingleCaseView.jsx
+++ src/jsx/components/SingleCaseView.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
@@ -6,154 +6,182 @@ import Download from "-!svg-react-loader?name=Logo!../../img/download-icon.svg";
 import Open from "-!svg-react-loader?name=Logo!../../img/open-details.svg";
 import Close from "-!svg-react-loader?name=Logo!../../img/close-details.svg";
 
-class SingleCaseView extends Component {
-	constructor() {
-		super();
-		this.state = {
-			showDetails: false,
-			loadingIframe: true
-		};
-	}
+const useAdobePreview = () => {
+	const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+	const adobeUIConfig = {
+		showLeftHandPanel: false,
+		showDownloadPDF: false,
+		showPrintPDF: false
+	};
 
-	hideShowDetails() {
-		this.setState({ showDetails: !this.state.showDetails });
-	}
-
-	hideLoadingIframe() {
-		this.setState({ loadingIframe: false }); // Explicit to false because is used in onLoad event
-	}
-
-	componentDidUpdate(prevProps) {
-		if (
-			this.props.singleCase &&
-			prevProps.singleCase &&
-			this.props.singleCase.pdf.pdfDbKey !== prevProps.singleCase.pdf.pdfDbKey
-		) {
-			this.setState({ loadingIframe: true });
+	useEffect(() => {
+		if (window.AdobeDC) {
+			setIsScriptLoaded(true);
+			return;
 		}
-	}
+		const script = document.createElement("script");
+		script.src = "https://documentcloud.adobe.com/view-sdk/main.js";
+		document.querySelector("body").appendChild(script);
+		document.addEventListener("adobe_dc_view_sdk.ready", () => setIsScriptLoaded(true));
+	}, []);
 
-	render() {
-		const { pdf = {}, caseCitations, cites, casesCitedsByCaseCited, legislationToCases } =
-			this.props.singleCase || {};
+	const getViewer = () => {
+		if (!isScriptLoaded) return;
 
-		return (
-			<div className="single-case-wrapper">
-				<div className="single-case-header">
-					<div className="details-open-close-button" onClick={this.hideShowDetails.bind(this)}>
-						info {this.state.showDetails ? <Close /> : <Open />}
-					</div>
-					<div className="download-button">
-						{pdf.pdfDbKey && (
-							<a href={`https://s3-ap-southeast-2.amazonaws.com/openlawnz-pdfs/${pdf.pdfDbKey}`} download>
-								<Download alt="Download" className="download-icon" />
-							</a>
-						)}
-					</div>
+		return new window.AdobeDC.View({
+			clientId: process.env.ADOBE_VIEW_KEY,
+			divId: "adobe-dc-view"
+		});
+	};
+
+	return { isScriptLoaded, getViewer, adobeUIConfig };
+};
+
+const SingleCaseView = props => {
+	const [showDetails, setShowDetails] = useState(false);
+	const [loadingIframe, setLoadingIframe] = useState(false);
+	const { isScriptLoaded, getViewer, adobeUIConfig } = useAdobePreview();
+	const { pdf = {}, caseName, caseCitations, cites, casesCitedsByCaseCited, legislationToCases } =
+		props.singleCase || {};
+	let adobeDCView;
+
+	const toggleShowDetails = () => {
+		setShowDetails(!showDetails);
+	};
+
+	useEffect(() => {
+		if (!isScriptLoaded || !pdf.pdfDbKey) return;
+
+		adobeDCView = getViewer();
+		adobeDCView.previewFile(
+			{
+				content: {
+					location: { url: `https://s3-ap-southeast-2.amazonaws.com/openlawnz-pdfs/${pdf.pdfDbKey}` }
+				},
+				metaData: { fileName: `${caseName}.pdf` }
+			},
+			adobeUIConfig
+		);
+		adobeDCView.registerCallback(
+			window.AdobeDC.View.Enum.CallbackType.EVENT_LISTENER,
+			({ type }) => type === "APP_RENDERING_START" && setLoadingIframe(false) // Set loading false when adobe loading start
+		);
+	}, [isScriptLoaded, pdf && pdf.pdfDbKey]);
+
+	useEffect(() => {
+		setLoadingIframe(true);
+	}, [props.isBeingUpdated]);
+
+	return (
+		<div className="single-case-wrapper">
+			<div className="single-case-header">
+				<div className="details-open-close-button" onClick={toggleShowDetails}>
+					info {showDetails ? <Close /> : <Open />}
 				</div>
-				<div className="row">
-					<div className="case-document-viewer">
-						{this.state.loadingIframe && (
-							<p className="loading-iframe">
-								<span className="spinner centered"></span>
-							</p>
-						)}
-						{pdf.pdfDbKey && (
-							<iframe
-								onLoad={this.hideLoadingIframe.bind(this)}
-								src={`https://docs.google.com/gview?url=https://s3-ap-southeast-2.amazonaws.com/openlawnz-pdfs/${pdf.pdfDbKey}&embedded=true`}
-								frameBorder={0}
-							/>
-						)}
-					</div>
-					<div
-						className={
-							this.state.showDetails
-								? this.props.isBeingUpdated
-									? "case-details loading"
-									: "case-details"
-								: "hide-case-details"
-						}
-					>
-						<h3 className="header">Citations known for this case</h3>
-						{caseCitations &&
-							(caseCitations === 0 ? (
-								<p>None</p>
-							) : (
-								<div role="listitem" className="item">
-									{caseCitations.map(obj => (
-										<div role="listitem" className="item" key={`cites-reference-${obj.citation}`}>
-											{obj.citation}
-										</div>
-									))}
-								</div>
-							))}
-						<hr></hr>
-
-						<h3 className="header">Cites</h3>
-						{cites &&
-							(cites.length === 0 ? (
-								<p>No cases</p>
-							) : (
-								<div role="listitem" className="item">
-									{cites.map(obj => (
-										<div role="listitem" className="item" key={`cites-reference-${obj.id}`}>
-											<Link to={`/case/${obj.id}`}>{obj.caseName}</Link>
-										</div>
-									))}
-								</div>
-							))}
-						<hr></hr>
-
-						<h3 className="header">Cited by</h3>
-						{casesCitedsByCaseCited &&
-							(casesCitedsByCaseCited.length === 0 ? (
-								<p>No cases</p>
-							) : (
-								<div role="listitem" className="item">
-									{casesCitedsByCaseCited.map(obj => (
-										<div
-											role="listitem"
-											className="item"
-											key={`cited-by-reference-${obj.caseByCaseOrigin.id}`}
-										>
-											<Link to={`/case/${obj.caseByCaseOrigin.id}`}>
-												{obj.caseByCaseOrigin.caseName}
-											</Link>
-										</div>
-									))}
-								</div>
-							))}
-						<hr></hr>
-
-						<h3 className="header">Legislation Referenced</h3>
-						{legislationToCases &&
-							(legislationToCases.length === 0 ? (
-								<p>No legislation</p>
-							) : (
-								<table cellSpacing="0" cellPadding="0">
-									<thead className="">
-										<tr className="">
-											<th className="title">Title</th>
-											<th className="section">Section</th>
-										</tr>
-									</thead>
-									<tbody className="">
-										{legislationToCases.map((obj, i) => (
-											<tr className="" key={`legislation-reference-${i}`}>
-												<td className="">{obj.legislation.title}</td>
-												<td className="">{obj.section}</td>
-											</tr>
-										))}
-									</tbody>
-								</table>
-							))}
-						<hr></hr>
-					</div>
+				<div className="download-button">
+					{pdf.pdfDbKey && (
+						<a href={`https://s3-ap-southeast-2.amazonaws.com/openlawnz-pdfs/${pdf.pdfDbKey}`} download>
+							<Download alt="Download" className="download-icon" />
+						</a>
+					)}
 				</div>
 			</div>
-		);
-	}
-}
+			<div className="row">
+				<div className="case-document-viewer">
+					{loadingIframe && (
+						<p className="loading-iframe">
+							<span className="spinner centered"></span>
+						</p>
+					)}
+					<div id="adobe-dc-view"></div>
+				</div>
+				<div
+					className={
+						showDetails
+							? props.isBeingUpdated
+								? "case-details loading"
+								: "case-details"
+							: "hide-case-details"
+					}
+				>
+					<h3 className="header">Citations known for this case</h3>
+					{caseCitations &&
+						(caseCitations === 0 ? (
+							<p>None</p>
+						) : (
+							<div role="listitem" className="item">
+								{caseCitations.map(obj => (
+									<div role="listitem" className="item" key={`cites-reference-${obj.citation}`}>
+										{obj.citation}
+									</div>
+								))}
+							</div>
+						))}
+					<hr></hr>
+
+					<h3 className="header">Cites</h3>
+					{cites &&
+						(cites.length === 0 ? (
+							<p>No cases</p>
+						) : (
+							<div role="listitem" className="item">
+								{cites.map(obj => (
+									<div role="listitem" className="item" key={`cites-reference-${obj.id}`}>
+										<Link to={`/case/${obj.id}`}>{obj.caseName}</Link>
+									</div>
+								))}
+							</div>
+						))}
+					<hr></hr>
+
+					<h3 className="header">Cited by</h3>
+					{casesCitedsByCaseCited &&
+						(casesCitedsByCaseCited.length === 0 ? (
+							<p>No cases</p>
+						) : (
+							<div role="listitem" className="item">
+								{casesCitedsByCaseCited.map(obj => (
+									<div
+										role="listitem"
+										className="item"
+										key={`cited-by-reference-${obj.caseByCaseOrigin.id}`}
+									>
+										<Link to={`/case/${obj.caseByCaseOrigin.id}`}>
+											{obj.caseByCaseOrigin.caseName}
+										</Link>
+									</div>
+								))}
+							</div>
+						))}
+					<hr></hr>
+
+					<h3 className="header">Legislation Referenced</h3>
+					{legislationToCases &&
+						(legislationToCases.length === 0 ? (
+							<p>No legislation</p>
+						) : (
+							<table cellSpacing="0" cellPadding="0">
+								<thead className="">
+									<tr className="">
+										<th className="title">Title</th>
+										<th className="section">Section</th>
+									</tr>
+								</thead>
+								<tbody className="">
+									{legislationToCases.map((obj, i) => (
+										<tr className="" key={`legislation-reference-${i}`}>
+											<td className="">{obj.legislation.title}</td>
+											<td className="">{obj.section}</td>
+										</tr>
+									))}
+								</tbody>
+							</table>
+						))}
+					<hr></hr>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default SingleCaseView;

b20394d5c65fa9321ef825a9404ea4159d6b8bef
Caio Navarro <caionavarro.n@hotmail.com>
2020-01-31 17:49:32 -0300

    Fix 'Court of Appeal' value because of case sensitive collation - AdvancedSearch

--- src/jsx/components/AdvancedSearch/Court.jsx
+++ src/jsx/components/AdvancedSearch/Court.jsx
@@ -11,7 +11,7 @@ const Court = ({ value, id, onChange, className }) => (
 		>
 			<option value="">Select an option</option>
 			<option value="Supreme Court">Supreme Court</option>
-			<option value="Court of Appeal">Court of Appeal</option>
+			<option value="Court of appeal">Court of Appeal</option>
 			<option value="High Court">High Court</option>
 			<option value="District Court">District Court</option>
 		</select>
